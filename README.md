c97e7cd8e602e31f14f86bcf3afb794c83b7f44c
Dean <dean@deansantillan.com>
2022-01-31 13:37:29 +1000

    tasks(): added html for log in and logout with google. installed passport and did setup ready for testing authentication.

--- README.md
+++ README.md
@@ -34,4 +34,28 @@
 ### OAuth 2.0
 > OAuth 2.0 is the industry-standard protocol for authorization. OAuth 2.0 focuses on client developer simplicity while providing specific authorization flows for web applications, desktop applications, mobile phones, and living room devices. This specification and its extensions are being developed within the IETF OAuth Working Group.
 >
+><hr>
+>
 > [Home Page](https://oauth.net/2/)
+
+### Passport Js
+> <strong style="color:green">Simple, unobtrusive authentication for Node.js</strong>
+>
+>Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.
+>
+> <hr>
+>
+> [Documentation](https://oauth.net/2/)
+
+
+### dotenv
+
+>
+>Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env. Storing configuration in the environment separate from code is based on The Twelve-Factor App methodology.
+> 
+> ``` 
+> yarn add dotenv 
+> ```
+>
+> [Documentation](https://github.com/motdotla/dotenv#readme)
+>
--- server.js
+++ server.js
@@ -3,21 +3,63 @@ import https from 'https';
 import path from 'path';
 import fs from 'fs';
 import helmet from 'helmet';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import { Strategy } from 'passport-google-oauth20';
+
+// authentication
+
+// server config
 const PORT = 4444;
-const app = express();
-
-// added security features to stop sniffing and hacking
-app.use(helmet())
-
 // keys to make server secure
 const securityKey = fs.readFileSync('key.pem');
 const securityCertificate = fs.readFileSync('cert.pem');
 
+// api keys
+dotenv.config();
+// google api keys
+const config = {
+	CLIENT_ID: process.env.CLIENT_ID,
+	CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+const AUTH_OPTIONS = {
+	callbackURL: '/auth/google/callback',
+	clientID: config.CLIENT_ID,
+	clientSecret: config.CLIENT_SECRET,
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+	console.log('Google profile:', profile);
+	done(null, profile);
+};
+
+// passport strategy
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+const app = express();
+// added security features to stop sniffing and hacking
+app.use(helmet());
+//authentication middleware passport
+app.use(passport.initialize());
+
+const validateUserCredentials = (req, res, next) => {
+	const isLoggedIn = true;
+	if (!isLoggedIn) {
+		return res.status(401).json({
+			error: 'You must log in to access this content! ',
+		});
+	}
+	next();
+};
+//
+app.get('/auth/google/', (req, res) => {});
+app.get('/auth/google/callback', (req, res) => {});
+app.get('/auth/logout', (req, res) => {});
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve('public', 'index.html'));
 });
 
-app.get('/secret', (req, res) => {
+app.get('/secret', validateUserCredentials, (req, res) => {
 	return res.send('Your personal secret value is "4444"');
 });
 
