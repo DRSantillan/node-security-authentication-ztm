836d2b116343e49996badb39d08e0a3f440627e4
Dean <dean@deansantillan.com>
2022-01-31 10:05:46 +1000

    tasks(): added helmet middleware for express security and dotenv to store secret values.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# node-security-authentication-ztm
+# Node.js Security & Authentication
+
+### Open SSL commands
+
+> OpenSSl command to generate SSL key and certficate
+> 
+> ```javascript 
+> openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 90 
+> ``` 
+>
+> [OpenSSl Documentation](https://www.openssl.org/docs/)
+> 
+> 
+
+### Helmet JS
+> Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
+> 
+> ``` 
+> yarn add helmet 
+> ```
+> 
+> ```javascript
+> import helmet from 'helmet';
+> // initialize helmet adding it to express
+> app.use(helmet());
+> ```
+> [Helmet Documentation](https://helmetjs.github.io/)
+
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import https from 'https';
 import path from 'path';
 import fs from 'fs';
-
+import helmet from 'helmet';
 const PORT = 4444;
 const app = express();
 
+// added security features to stop sniffing and hacking
+app.use(helmet())
+
+// keys to make server secure
 const securityKey = fs.readFileSync('key.pem');
 const securityCertificate = fs.readFileSync('cert.pem');
 
@@ -17,11 +21,15 @@ app.get('/secret', (req, res) => {
 	return res.send('Your personal secret value is "4444"');
 });
 
+// using https and express for added features and configurability
 https
-	.createServer({
-		key: securityKey,
-		cert: securityCertificate,
-	}, app)
+	.createServer(
+		{
+			key: securityKey,
+			cert: securityCertificate,
+		},
+		app
+	)
 	.listen(PORT, () => {
 		console.log(`Server listening on Port: ${PORT}`);
 	});
